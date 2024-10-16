f7a33565c519192d159699b68fcb8d5a5ae4c541
Joseph_MCV <josephmauriziocontrerasvila@gmail.com>
2024-10-16 18:16:48 -0500

    Add patient schemas and routes and modify database model

--- FastApi/app/config/database.py
+++ FastApi/app/config/database.py
@@ -2,7 +2,7 @@
 This module sets up the database connection and defines the DentistModel.
 """
 
-from peewee import Model, MySQLDatabase, AutoField, CharField, IntegerField
+from peewee import Model, MySQLDatabase, AutoField, CharField, IntegerField,DateField
 from config.settings import DATABASE
 
 database = MySQLDatabase(
@@ -35,3 +35,23 @@ class DentistModel(Model):
 
         database = database
         table_name = "dentists"
+
+class PatientModel(Model):
+    """
+    PatientModel represents a patient entity in the database.
+    """
+
+    id_patient = AutoField(primary_key=True)
+    name = CharField(max_length=40)
+    last_name = CharField(max_length=40)
+    address = CharField(max_length=100)
+    departure_date = DateField(null=True) 
+    id_user = IntegerField(null=True)      
+    dni = IntegerField(null=True)          
+
+    class Meta:
+        """
+        Meta class for configuring the database settings.
+        """
+        database = database
+        table_name = "patients"
