6fc8b85a8c0641cf76e65ebe7d1e24c856375cd9
Mateo Loaiza <loaiza.mateo.7291@eam.edu.co>
2024-10-20 20:40:29 -0500

    Update main and small fixes

--- FastApi/app/schemas/dentist.py
+++ FastApi/app/schemas/dentist.py
@@ -12,7 +12,7 @@ class Dentist(BaseModel):
       id_dentist (int): Unique identifier for the dentist.
       name (str): First name of the dentist, with a maximum length of 40 characters.
       lastname (str): Last name of the dentist, with a maximum length of 40 characters.
-      licence (str): License number of the dentist, with a maximum length of 40 characters.
+      license (str): License number of the dentist, with a maximum length of 40 characters.
       id_state (int): Identifier for the state where the dentist is licensed.
       inactive_days (int): Number of inactive days for the dentist.
       id_user (int): Identifier for the associated user.
@@ -24,7 +24,7 @@ class Dentist(BaseModel):
     id_dentist: int
     name: str = Field(..., max_length=40)
     last_name: str = Field(..., max_length=40)
-    licence: str = Field(..., max_length=40)
+    license: str = Field(..., max_length=40)
     id_state: int
     inactive_days: int
     id_user: int
