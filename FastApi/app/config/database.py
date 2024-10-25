35da7f336213cfd057f0d2c66c1384aff153fbf0
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-10-25 08:51:20 -0500

    Update database.py (#26)

--- FastApi/app/config/database.py
+++ FastApi/app/config/database.py
@@ -2,8 +2,11 @@
 This module sets up the database connection and defines the DentistModel.
 """
 
-from peewee import Model, MySQLDatabase, AutoField, CharField, IntegerField, DateField
+from peewee import Model, MySQLDatabase, AutoField, CharField, IntegerField, DateField,ForeignKeyField,  DateTimeField
 from config.settings import DATABASE
+
+from datetime import datetime 
+import pytz
 
 database = MySQLDatabase(
     DATABASE["name"],
@@ -13,6 +16,69 @@ database = MySQLDatabase(
     port=DATABASE["port"],
 )
 
+Colombian = pytz.timezone('America/Bogota')
+
+class RoleModel(Model):
+    """
+    RoleModel represents a role entity in the database.
+    """
+    id_role = IntegerField()
+    name = CharField(max_length=40)
+    description = CharField(max_length=100)
+
+    class Meta:
+        """
+        Meta class for configuring the database settings.
+        """
+        database = database
+        table_name = "roles"
+
+class UserModel(Model):
+    """
+    UserModel represents a user entity in the database.
+    """
+    
+    id_user = AutoField(primary_key=True)
+    email = CharField(max_length=200, unique=True)
+    password = CharField(max_length=200)
+    id_role = ForeignKeyField(RoleModel, backref='user')
+
+    class Meta:
+        """
+        Meta class for configuring the database settings.
+        """
+        database = database
+        table_name = "users"
+
+class ScheduleDentistModel(Model):
+    """
+    ScheduleDentistModel represents a schedule entity in the database.
+    """
+    
+    id_schedule = AutoField(primary_key=True)
+    day_start_time = DateTimeField(
+        default=lambda: datetime.now(pytz.timezone(Colombian)))
+    end_time_day = DateTimeField(
+        default=lambda: datetime.now(pytz.timezone(Colombian)))
+
+    class Meta:
+        """
+        Meta class for configuring the database settings.
+        """
+        database = database
+        table_name = "Schedule"
+
+class StateDentistModel(Model):
+    id_state = AutoField(primary_key=True)
+    name = CharField(max_length=40)
+    description = CharField(max_length=100)
+
+    class Meta:
+        """
+        Meta class for configuring the database settings.
+        """       
+        database = database
+        table_name = "states"
 
 class DentistModel(Model):
     """
@@ -23,10 +89,10 @@ class DentistModel(Model):
     name = CharField(max_length=40)
     last_name = CharField(max_length=40)
     license = CharField(max_length=40)
-    id_state = IntegerField(null=True)
+    id_state = ForeignKeyField(StateDentistModel, backref='dentist')
     inactive_days = IntegerField(null=True)
-    id_user = IntegerField(null=True)
-    id_schedule = IntegerField(null=True)
+    id_user = ForeignKeyField(UserModel, backref='dentist')
+    id_schedule = ForeignKeyField(ScheduleDentistModel, backref='dentist')
 
     class Meta:
         """
@@ -47,7 +113,7 @@ class PatientModel(Model):
     last_name = CharField(max_length=40)
     address = CharField(max_length=100)
     departure_date = DateField(null=True)
-    id_user = IntegerField(null=True)
+    id_user = ForeignKeyField(UserModel, backref='patient')
     dni = IntegerField(null=True)
 
     class Meta:
@@ -58,13 +124,41 @@ class PatientModel(Model):
         database = database
         table_name = "patients"
 
-class ShiftModel(Model):
+
+
+class labelAppointmentModel(Model):
     """
-    ShiftModel represents a shift entity in the database.
+    labelAppointment represents a labelAppointment entity in the database.
     """
-    id_shift = AutoField(primary_key=True)
-    date = DateField(null=True)
-    hour = IntegerField(null=True)
-    label = IntegerField(null=True)
-    id_patient = IntegerField(null= True)
-    id_dentist = IntegerField(null=True)
+    id_label = AutoField(primary_key=True)
+    name = CharField(max_length=100)
+    hour = DateTimeField(
+        default=lambda: datetime.now(pytz.timezone(Colombian)))
+
+    class Meta:
+        """
+        Meta class for configuring the database settings.
+        """
+
+        database = database
+        table_name = "labelAppointment"
+
+
+class AppointmentModel(Model):
+    """
+    Appointment represents a Appointment entity in the database.
+    """
+    id_appointment = AutoField(primary_key=True)
+    date = DateTimeField(
+        default=lambda: datetime.now(pytz.timezone(Colombian)))
+    id_label = ForeignKeyField(labelAppointmentModel, backref='appoinment')
+    id_patient = ForeignKeyField(PatientModel, backref='appoinment')
+    id_dentist = ForeignKeyField(DentistModel, backref='appoinment')
+
+    class Meta:
+        """
+        Meta class for configuring the database settings.
+        """
+
+        database = database
+        table_name = "appoinments"
